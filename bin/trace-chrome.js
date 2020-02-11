da8888c91cb21c475d1b5c088da3ae137acc37df
José Dapena Paz <jose.dapena@lge.com>
2020-02-11 18:11:25 +0100

    Add memory-infra periodic snapshot support

--- bin/trace-chrome.js
+++ bin/trace-chrome.js
@@ -6,6 +6,10 @@ program.option('-H, --host <host>', 'Remote debugging protocol host', 'localhost
        .option('-s, --showcategories', 'Show categories')
        .option('-c, --categories <categories>', 'Set categories', "")
        .option('-e, --excludecategories <categories>', 'Exclude categories', "")
+       .option('--systrace', 'Enable systrace')
+       .option('--memory_dump_mode <mode>', 'Memory dump mode', "")
+       .option('--memory_dump_interval <interval_in_ms>', 'Memory dump interval in ms', 2000)
+       .option('--dump_memory_at_stop')
        .parse(process.argv);
 
 var options = {
@@ -13,8 +17,21 @@ var options = {
     'port': program.port,
 };
 
+var dumpInterval;
+
 Chrome(options, function (chrome) {
     with (chrome) {
+
+	function dump_memory() {
+	    Tracing.requestMemoryDump().then(
+		function() {
+		    console.error("Memory dump done");
+		},
+		function() {
+		    console.error("Memory dump failed");
+		}
+	    );
+	}
 
 	if (program.showcategories) {
 	    Tracing.getCategories(function (message, result) {
@@ -33,6 +50,13 @@ Chrome(options, function (chrome) {
 		close();
             });
             process.on('SIGINT', function() {
+		if (dumpInterval) {
+		    clearInterval(dumpInterval);
+		}
+		if (program.dump_memory_at_stop) {
+		    console.error("Dumping memory at stop");
+		    dump_memory();
+		}
 		Tracing.end();
             });
 	    console.error("Connecting to: "+program.host+":"+program.port);
@@ -45,6 +69,21 @@ Chrome(options, function (chrome) {
 		traceConfig["excludedCategories"] = program.excludecategories.split(",");
 	        console.error("Excluded categories: "+ program.excludecategories);
 	    }
+	    if (program.systrace) {
+		traceConfig["enable_systrace"] = true;
+	    }
+	    if (program.memory_dump_mode) {
+		traceConfig["memory_dump_config"] = {
+		    "triggers": [
+			{
+			    "mode": program.memory_dump_mode,
+			    "periodic_interval_ms": program.memory_dump_interval
+			}
+		    ]
+		}
+		dumpInterval = setInterval(dump_memory, program.memory_dump_interval);
+	    }
+	    console.error("Traceconfig is " + JSON.stringify(traceConfig));
             Tracing.start({"traceConfig" : traceConfig});
 	}
     }
